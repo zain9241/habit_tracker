c097552a05d032c7913a2924f7e1a84c6aeee82c
zain9241 <zainsaqib770.com>
2026-07-17 00:13:06 +0500

    API integration in Country List Screen

--- lib/country_list.dart
+++ lib/country_list.dart
@@ -1,19 +1,17 @@
-// country_list.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 Future<List<String>> fetchCountries() async {
-  List<String> countries = [
-    'United States',
-    'Canada',
-    'United Kingdom',
-    'Australia',
-    'India',
-    'Germany',
-    'France',
-    'Japan',
-    'China',
-    'Brazil',
-    'South Africa'
-  ];
+  final response = await http
+      .get(Uri.parse('https://date.nager.at/api/v3/AvailableCountries'));
 
-  return countries;
+  if (response.statusCode == 200) {
+    List<dynamic> countriesJson = json.decode(response.body);
+    List<String> countryList =
+    countriesJson.map((country) => country['name'] as String).toList();
+    countryList.sort();
+    return countryList;
+  } else {
+    throw Exception('Failed to load countries');
+  }
 }
